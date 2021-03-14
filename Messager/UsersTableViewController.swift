7ea968dcc7b8165d48a4c00447ff541a54f0926d
Chongbei Wang <chongbei@amazon.com>
2021-03-13 18:04:30 -0800

    034 Searching Users

--- Messager/UsersTableViewController.swift
+++ Messager/UsersTableViewController.swift
@@ -7,27 +7,69 @@ class UsersTableViewController: UITableViewController {
     var filteredUsers: [User] = []
     
     let searchController = UISearchController(searchResultsController: nil)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //createDummyUsers()
+        tableView.tableFooterView = UIView()
+        setupSearchController()
+        downloadUsers()
     }
-
+    
     // MARK: UICollectionViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchController.isActive ? filteredUsers.count : allUsers.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! UserTableViewCell
-
+        
         let user = searchController.isActive ? filteredUsers[indexPath.row] : allUsers[indexPath.row]
-
+        
         cell.configure(user: user)
-
+        
         return cell
     }
- 
+    
+    
+    //MARK: - DownloadUsers
+    
+    private func downloadUsers() {
+        FirebaseUserListener.shared.downloadAllUsersFromFirebase {(allFirebaseUsers) in
+            
+            self.allUsers = allFirebaseUsers
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: setupSearchController
+    private func setupSearchController() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search user"
+        searchController.searchResultsUpdater = self
+        definesPresentationContext = true
+    }
+    
+    private func filteredContentForSearchText(searchText: String) {
+        filteredUsers = allUsers.filter({(user) -> Bool in
+            return user.userName.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+}
 
+extension UsersTableViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    
 }
