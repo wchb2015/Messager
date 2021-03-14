3239b927c3a0fd8205988c55a9ccd47713133ce0
Chongbei Wang <chongbei@amazon.com>
2021-03-13 22:06:49 -0800

    036 Profile View UI

---
+++ Messager/SecondaryViews/ProfileTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewController.swift
+//  Messager
+//
+//  Created by Wang, Chongbei on 3/13/21.
+//  Copyright © 2021 com.wchb. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    //MARK: ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
